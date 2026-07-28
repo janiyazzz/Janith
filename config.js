07a67716bdcd755133fbbde51bfd439d234af5e8
janiyazzz <janithsachintha54@gmail.com>
2026-07-28 20:25:40 +0530

    Update bot configuration in config.js
    Updated bot configuration settings including owner number and bot name. Changed various boolean settings to string representations.

--- config.js
+++ config.js
@@ -1,36 +1,37 @@
 module.exports = {
     PREFIX: ".",
-    OWNER_NUMBER: "94761846512",
-    BOT_NAME: "BLACK-QUEEN-MD",
+    OWNER_NUMBER: "94701413155",
+    BOT_NAME: "JANIYAZZZ-MD",
+    ALIVE_MSG: "Hello, JANIYAZZZ-MD is Online Now! 🖤",
     WORK_TYPE: "public",
 
-    AUTO_TYPING: false,
-    AUTO_RECORDING: true,
-    ALWAYS_ONLINE: false,
-    ALWAYS_OFFLINE: true,
-    AUTO_READ: false,
-    READ_CMD_ONLY: true,
-    AUTO_BIO: true,
+    AUTO_TYPING: "Off❌",
+    AUTO_RECORDING: "On✅",
+    ALWAYS_ONLINE: "Off❌",
+    ALWAYS_OFFLINE: "On✅",
+    AUTO_READ: "Off❌",
+    READ_CMD_ONLY: "On✅",
+    AUTO_BIO: "off❌",
 
-    AUTO_REACT: false,
+    AUTO_REACT: "off❌",
     REACT_EMOJIS: ['❤️','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
 
-    AUTO_READ_STATUS: true,
-    AUTO_VIEW_STATUS: true,
-    AUTO_LIKE_STATUS: true,
+    AUTO_READ_STATUS: "On✅",
+    AUTO_VIEW_STATUS: "On❌",
+    AUTO_LIKE_STATUS: "On✅",
     AUTO_LIKE_EMOJI: ['❤️', '🔥', '👍', '😂', '😮'],
 
-    ANTI_BOT: false,
-    ANTI_BAD: false,
-    ANTI_LINK: false,
-    ANTI_DELETE: "true",
+    ANTI_BOT: "Off❌",
+    ANTI_BAD: "off❌",
+    ANTI_LINK: "off❌",
+    ANTI_DELETE: "on✅",
     
     // පහත පේළි දෙක අලුතින් එකතු කරන්න
     ANTI_DEL_PATH: "same", 
-    AUTO_RECORDING: "false",
+    AUTO_RECORDING: "Off❌",
 
     NEWSLETTER_JID: "7271916507180@lid",
-    GROUP_INVITE_LINK: "https://chat.whatsapp.com/CqDoPKbD49jBxiGb2fLUdt?s=cl&p=a&ilr=0",
+    CHANNEL_INVITE_LINK: "https://whatsapp.com/channel/0029VbDpDlRKwqSOg23SlN2Z",
     MAX_RETRIES: 3,
     RCD_IMAGE_PATH: "https://i.ibb.co/R4tZrHYC/5aedfbc8f1b5.png"
 };
