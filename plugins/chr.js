cebab83caed96eba91d80d547f7d91efa662574b
janiyazzz <janithsachintha54@gmail.com>
2026-07-29 18:24:06 +0530

    Update copyright notice in chr.js

--- plugins/chr.js
+++ plugins/chr.js
@@ -52,7 +52,7 @@ async (conn, mek, m, { from, q, reply, pushname }) => {
 │
 ╰───────────────⟡
 
-> © 𝙱𝙻𝙰𝙲𝙺 𝚀𝚄𝙴𝙴𝙽 𝙼𝙳 𝚅1`;
+> © 𝙹𝙰𝙽𝙸𝚈𝙰𝚉𝚉 𝙼𝙳 𝚅1`;
 
         await reply(caption);
         await conn.sendMessage(from, { react: { text: "✅", key: mek.key } });
